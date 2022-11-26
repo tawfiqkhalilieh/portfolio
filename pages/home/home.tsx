55061588861fa284bc835c477267a1936e14c2e7
xtawfeeq7k <taw.coding@gmail.com>
2022-11-26 12:27:59 +0200

    start encancing the ui

--- pages/home/home.tsx
+++ pages/home/home.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Fade } from "react-awesome-reveal";
-import { VFooter, VMainHeader, VMenuBar, VToolsTech } from "../../utils";
+import { VFooter, VMainHeader, VMenuBar } from "../../utils";
+import VToolsTech from "../../utils/home/toolstech";
 import { chevronDown } from "../../assets";
 import { profilePicture } from "../../assets";
 import styles from "./home.module.scss";
 import Head from "next/head";
+import techs from "../../utils/techs";
 
 export default function VHome() {
   return (
@@ -38,32 +40,62 @@ export default function VHome() {
             </a>
           </div>
         </div>
-        <div className={styles.workHistory}>
-          <div className={styles.linkBox}>
-            <Fade>
-              <h1 className={styles.linkBoxTitle} id="WorkHistory">
-                Work
-                <br />
-                History
-              </h1>
-              <h2 className={styles.linkBoxTextContent}>
-                Here, you can learn more about the companies I&apos;ve
-                collaborated with, projects I&apos;ve been part of, and the
-                technologies I use every day.
-              </h2>
-              <Link href={`/work`}>
-                <a className={styles.linkBoxButton}>
-                  Check out my work history
-                </a>
-              </Link>
-            </Fade>
-          </div>
+        <></>
+        <div className={styles.linkBoxes}>
           <div className={styles.workHistoryBackground} />
+          <div className={styles.boxesContaier}>
+            <div className={styles.linkBox}>
+              <Fade>
+                <h1 className={styles.linkBoxTitle} id="WorkHistory">
+                  Work
+                  <br />
+                  History
+                </h1>
+                <h2 className={styles.linkBoxTextContent}>
+                  Here, you can learn more about the companies I&apos;ve
+                  collaborated with, projects I&apos;ve been part of, and the
+                  technologies I use every day.
+                </h2>
+                <Link href={`/work`}>
+                  <a className={styles.linkBoxButton}>
+                    Check out my work history
+                  </a>
+                </Link>
+              </Fade>
+            </div>
+
+            <div className={styles.linkBox}>
+              <Fade>
+                <h1 className={styles.linkBoxTitle} id="showcase">
+                  Showcase
+                </h1>
+                <h2 className={styles.linkBoxTextContent}>
+                  Here, you can see a couple of the projects I&apos;ve been
+                  working on last two years.
+                </h2>
+                <Link href={`/showcase`}>
+                  <a className={styles.linkBoxButton}>Check out my showcase</a>
+                </Link>
+              </Fade>
+            </div>
+          </div>
         </div>
+
         <div className={styles.technologiesAndTools}>
           <Fade>
             <h1>Technologies & Tools I work with</h1>
-            <VToolsTech />
+            <h2 className={styles.tool_tech_header}>
+              - Backend : <VToolsTech tech={techs.backend} />
+            </h2>
+            <h2 className={styles.tool_tech_header}>
+              - Frontend : <VToolsTech tech={techs.frontend} />
+            </h2>
+            <h2 className={styles.tool_tech_header}>
+              - Automation : <VToolsTech tech={techs.automation} />
+            </h2>
+            <h2 className={styles.tool_tech_header}>
+              - Tools : <VToolsTech tech={techs.tools} />
+            </h2>
           </Fade>
         </div>
       </div>
---
+++ utils/techs.ts
@@ -0,0 +1,9 @@
+import React from "react";
+enum techs {
+  frontend = "frontend",
+  backend = "backend",
+  automation = "automation",
+  tools = "tools",
+}
+
+export default techs;
