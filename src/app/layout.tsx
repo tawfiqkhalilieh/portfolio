07ea4be332b87ee8f006302ea230d10c7e25f168
tawfiqkhalilieh <taw.coding@gmail.com>
2025-10-27 09:41:38 +0200

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,13 +30,6 @@ export const metadata: Metadata = {
         "UI/UX",
         "iHelp",
         "Loop"
-    ],
-    icon: [
-        { rel: "icon", url: "/favicon.ico" },
-        { rel: "apple-touch-icon", url: "/apple-touch-icon.png", sizes: "180x180" },
-        { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-        { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-        { rel: "manifest", url: "/site.webmanifest" },
     ]
 };
 
